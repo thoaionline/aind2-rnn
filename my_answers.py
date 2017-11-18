922984b3de15897a1d6f76877192eed29c0d0d77
Thoai Nguyen <me@thoaionline.com>
2017-11-18 16:50:08 +0000

    Implement window_transform_series

--- my_answers.py
+++ my_answers.py
@@ -6,19 +6,16 @@ from keras.layers import LSTM
 import keras
 
 
-# TODO: fill out the function below that transforms the input series 
-# and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
-    # containers for input/output pairs
-    X = []
-    y = []
+    X = list(map(lambda i:series[i:i+window_size],range(0,len(series) - window_size)))
+    y = series[window_size:]
 
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
-
+    
     return X,y
 
 # TODO: build an RNN to perform regression on our time series input/output data
