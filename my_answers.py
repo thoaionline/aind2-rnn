00c919b843bc8521f689605888b4472e6c012212
Thoai Nguyen <me@thoaionline.com>
2017-11-18 17:27:22 +0000

    Implement part 1

--- my_answers.py
+++ my_answers.py
@@ -20,7 +20,11 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(window_size, input_shape=(window_size,1)))
+    
+    model.add(Dense(1))
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
